38385a9c125136bc64989a134eedc5e1fa89113c
Nick Bakkegaard <nickbakkegaard@gmail.com>
2020-07-10 17:39:59 +0200

    go fmt

--- src/prime.go
+++ src/prime.go
@@ -5,34 +5,34 @@ import "fmt"
 import "strconv"
 import "math"
 
-func isPrime(n int) bool{
-	if n%2==0 {
+func isPrime(n int) bool {
+	if n%2 == 0 {
 		return false
 	}
-	for i := 3;i<int(math.Sqrt(float64(n)))+1;i+=2 {
-		if n%i==0 {
+	for i := 3; i < int(math.Sqrt(float64(n)))+1; i += 2 {
+		if n%i == 0 {
 			return false
 		}
 	}
 	return true
 }
 
-func main(){
-	if len(os.Args)<2 {
-		fmt.Println("Requires one argument, remember target");
+func main() {
+	if len(os.Args) < 2 {
+		fmt.Println("Requires one argument, remember target")
 	}
-	var sum int64= 0
-	var target, error = strconv.ParseInt(os.Args[1],10,64)
+	var sum int64 = 0
+	var target, error = strconv.ParseInt(os.Args[1], 10, 64)
 
 	//Stupid...
 	_ = error
 
 	var target2 = int(target)
 
-	for i :=2;i<target2;i++ {
+	for i := 2; i < target2; i++ {
 		if isPrime(i) {
-			sum++;
+			sum++
 		}
 	}
-	fmt.Println(sum);
+	fmt.Println(sum)
 }
